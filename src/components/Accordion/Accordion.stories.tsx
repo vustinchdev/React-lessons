9abf5479be4a2c06d793fc35ba8ed5c7f2ba1417
vustinchdev <vustinchdev@gmail.com>
2023-10-23 19:38:47 +0300

    added Accordion.stories

---
+++ src/components/Accordion/Accordion.stories.tsx
@@ -0,0 +1,38 @@
+// Button.stories.ts|tsx
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Accordion from './Accordion';
+import { useState } from 'react';
+import { action } from '@storybook/addon-actions'
+
+export default {
+    component: Accordion,
+}
+
+
+const onChangeCallback = action('onCahnge')
+
+export const CollapsedAccordion = () => {
+    return <Accordion
+        onClick={onChangeCallback}
+        colapsed={true}
+        titleValue='Collapsed Accordion' />
+}
+
+export const OpenedAccordion = () => {
+    return <Accordion
+        onClick={() => { }}
+        colapsed={true}
+        titleValue='Opened Accordion' />
+}
+
+
+export const AccordionDemo = () => {
+
+    const [collapsed, setCollapsed] = useState(false)
+    return <Accordion
+        onClick={() => setCollapsed(!collapsed)}
+        colapsed={false}
+        titleValue='Opened Accordion' />
+}
